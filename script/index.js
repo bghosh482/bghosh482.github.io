56bdeefd36d989b307603522df3a9b5391c12743
Bubai Ghosh <bghosh482@gmail.com>
2022-09-28 02:54:12 +0530

    update

--- script/index.js
+++ script/index.js
@@ -1,32 +1,31 @@
+let navbar = document.querySelector(".navbar");
 
+window.addEventListener("scroll", function () {
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
 
- const navbar = document.querySelector("navbar");
+let menu = document.querySelector("#menu-icon");
+let navbar_link = document.querySelector(".navbar-link");
 
- window.addEventListener ("scroll", function() {
-     navbar.classList.toggle ("sticky", window.scrollY >0);
- });
- 
- let menu = document.querySelector('#menu-icon');
- let navbar_link = document.querySelector('.navbar-link');
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar_link.classList.toggle("active");
+};
 
- 
- menu.onclick = () => {
-     menu.classList.toggle('bx-x');
-     navbar_link.classList.toggle('active');
- };
- 
- window.onscroll = () => {
-     menu.classList.remove('bx-x');
-     navbar_link.classList.remove('active');
- };
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar_link.classList.remove("active");
+};
 
+const sr = ScrollReveal({
+  distance: "25px",
+  duration: 1000,
+  reset: true,
+});
 
- const sr = ScrollReveal ({
-	distance: '25px',
-	duration: 1000,
-	reset: true
-})
+sr.reveal(".home-text", { delay: 100, origin: "bottom" });
 
-sr.reveal('.home-text',{delay:100, origin:'bottom'})
-
-sr.reveal('.about,.contact,.Skills_container,.project-section,#statictics',{delay:100, origin:'top'})
+sr.reveal(".about,.contact,.Skills_container,.project-section,#statictics", {
+  delay: 100,
+  origin: "top",
+});
