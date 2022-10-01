d507d46bf8838604a206a4008ec90280b20fb8dc
Bubai Ghosh <bghosh482@gmail.com>
2022-10-02 04:47:20 +0530

    working contact form

--- script/index.js
+++ script/index.js
@@ -37,6 +37,7 @@ function sendEmail() {
   let params = {
     from_name: document.getElementById("fullName").value,
     email_id: document.getElementById("email_id").value,
+    subject: document.getElementById("subject").value,
     message: document.getElementById("message").value,
   };
 
